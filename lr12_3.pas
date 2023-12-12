55ca9798247b49b7bcdd59be1626b0ba8a2045d8
DeVayle <144678017+DeVayle@users.noreply.github.com>
2023-12-12 18:54:41 +0300

    Add files via upload

---
+++ lr12_3.pas
@@ -0,0 +1,13 @@
+﻿program lr12_3;
+
+var
+  pon: text;
+  s: string;
+  
+begin
+  read(s);
+  assign(pon, 'd:\pon.txt');
+  append(pon); //дополнение файла
+  writeln(pon, s);
+  close(pon);
+end.
---
+++ lr12_7.pas
@@ -0,0 +1,23 @@
+﻿program lr12_7;
+
+var
+  z31, z32: text;
+  i, j, k, n, s: integer;
+  
+begin
+  assign(z31, 'd:\z3.in.txt');
+  reset(z31);
+  readln(z31, n);
+  close(z31);
+  for i:=1 to n do
+  begin
+    k:=0;
+    for j:=1 to i do
+    if (i mod j = 0) then inc(k);
+    if k = 5 then s+=i;
+    end;
+  assign(z32, 'd:\z3.out');
+  rewrite(z32);
+  write(z32, s);
+  close(z32);
+end.
